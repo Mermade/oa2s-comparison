913382944e02a62b71e9e55508e6682a7ab39d98
Mike Ralphson <mike.ralphson@gmail.com>
2016-12-17 13:16:53 +0000

    Try with bundled js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var swaggerToSlate = require('swagger-to-slate/src/convert.js');
 swaggerToSlate('./specs/petstore.json','./output/swagger-to-slate.md');
 
 var inputStr = fs.readFileSync('./output/swagger-to-slate.md','utf8');
-shins.render(inputStr,{},function(err,str){
+
+var options = {};
+options.minify = true;
+
+shins.render(inputStr,options,function(err,str){
   fs.writeFileSync('./docs/swagger-to-slate.html',str,'utf8');
 });
